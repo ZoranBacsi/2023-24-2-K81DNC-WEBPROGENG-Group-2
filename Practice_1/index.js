85916f1b14fa5d27ec959398ae84ed9a304d3fe0
Zoran Heinczinger <zoran.heinczinger@gmail.com>
2024-02-14 23:03:41 +0100

    Practice 1 - solution

--- Practice_1/index.js
+++ Practice_1/index.js
@@ -24,33 +24,43 @@ myFunction(fruit)
 // console.log(fruits.map(d => typeof(d)))
 
 let movies = [{
-    "title": "Stargate - Original series",
-    "length": "45:00",
-    "year": 1996,
-    "director": "Direct Ores",
-    "actors": ["Richard Dean Anderson","fdsfsd"]
+    "title": "Stargate",
+    "length": "121",
+    "year": 1994,
+    "director": "Roland Emmerich",
+    "actors": ["Kurt Russell","James Spades","Jaye Davidson","Viveca Lindfors"]
 },{
-    "title": "Stargate - Original series",
-    "length": "45:00",
-    "year": 1996,
-    "director": "Direct Ores",
-    "actors": ["Richard Dean Anderson","fdsfsd"]
+    "title": "Stargate - The Ark of Truth",
+    "length": "102",
+    "year": 2008,
+    "director": "Robert C. Cooper",
+    "actors": ["Ben Bowder","Amanda Tapping", "Christopher Judge","Michael Shanks","Claudia Black","Beau Bridges"]
 },{
-    "title": "Stargate - Original series",
-    "length": "45:00",
-    "year": 1996,
-    "director": "Direct Ores",
-    "actors": ["Richard Dean Anderson","fdsfsd"]
+    "title": "Stargate - Continuum",
+    "length": "98",
+    "year": 2008,
+    "director": "Martin Wood",
+    "actors": ["Ben Bowder","Amanda Tapping","Christopher Judge","Michael Shanks","Claudia Black","Beau Bridges","Richard Dean Anderson"]
 },{
-    "title": "Stargate - Original series",
-    "length": "45:00",
-    "year": 1996,
-    "director": "Direct Ores",
-    "actors": ["Richard Dean Anderson","fdsfsd"]
+    "title": "Stargate - SG-1",
+    "length": "0",
+    "year": 1998,
+    "director": "Brad Wright",
+    "actors": ["Richard Dean Anderson","Michael Shanks","Amanda Tapping","Christopher Judge","Don S. Davis"]
 }
 ]
 
 //A) List of movies(title - year)
+let movieList = []
+movies.forEach(f => {
+    movieList.push([f["title"],f["year"]])
+})
+console.log(movieList.join(" , "))
 //B) Movie with Richard Dean Anderson
+console.log(movies.filter(e => e["actors"].includes("Richard Dean Anderson")))
 //C) Which is the longest movie
-
+let longestMovie = movies[0]
+movies.forEach(f => {
+    if(f["length"] >> longestMovie["length"]) longestMovie = f
+})
+console.log(longestMovie)
