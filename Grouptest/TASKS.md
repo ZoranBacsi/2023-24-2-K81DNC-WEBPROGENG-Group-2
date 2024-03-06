048b727ab3e6d78b116616dfbd16e8c723deda95
Zoran Heinczinger <zoran.heinczinger@gmail.com>
2024-03-06 15:54:19 +0100

    update gitignore file

---
+++ Grouptest/TASKS.md
@@ -0,0 +1,18 @@
+# Javascript Group Test
+
+Today, the world's largest digital game platform hired us to solve the open tickets/projects with the help of our most qualified programmers, which the agile scrum masters selected from the backlog.
+
+Our task is to create statistics about popular games.
+
+- **a)** 3 point(s): Is it true that every game is cheaper than 60 euros? (`price`)
+- **b)** 3 point(s): Give a game (`name`) that was played by at least 1,000,000 people at the same time today! (`peak`)
+- **c)** 3 point(s): Which game (`name`) was played by the fewest people today? (`peak`)
+- **d)** 3 point(s): List the free games! (`name`, `price: 0`)
+
+![statistics](src/statistics.jpg)
+
+Mark the tasks you solved:
+- [ ] a.
+- [ ] b.
+- [ ] c.
+- [ ] d.
---
+++ Grouptest/data.js
@@ -0,0 +1,50 @@
+const games = [
+    {
+        name: "Baldur's Gate 3",
+        price: 59.99,
+        peak: 267000,
+        genre: ['rpg', 'adventure', 'story rich']
+    },
+    {
+        name: "Civilization VI",
+        price: 59.99,
+        peak: 70000,
+        genre: ['strategy', 'turn-based', 'historical']
+    },
+    {
+        name: "Bloons TD 6",
+        price: 13.79,
+        peak: 13000,
+        genre: ['tower defense', 'singleplayer', 'online co-op', 'multiplayer', 'strategy']
+    },
+    {
+        name: "Dota 2",
+        price: 0.0,
+        peak: 666000,
+        genre: ['free to play', 'MOBA', 'multiplayer']
+    },
+    {
+        name: "Grand Theft Auto 5",
+        price: 39.98,
+        peak: 203000,
+        genre: ['open world', 'action', 'crime']
+    },
+    {
+        name: "Counter-Strike 2",
+        price: 0.0,
+        peak: 1156000,
+        genre: ['fps', 'shooter', 'multiplayer', 'competitive']
+    },
+    {
+        name: "Hearts of Iron IV",
+        price: 39.99,
+        peak: 49000,
+        genre: ['world war ii', 'strategy', 'historical', 'war']
+    },
+    {
+        name: "Fallout 4",
+        price: 19.99,
+        peak: 24000,
+        genre: ['open world', 'post-apocalyptic', 'exploration', 'rpg']
+    },
+]
